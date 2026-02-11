81f603db4fc6d57947f3c0550a745c1ec5471988
root <loay1alhindi1@gmail.com>
2026-02-11 21:30:15 +0300

    fix: unused other

--- src/utils/SessionManager.cpp
+++ src/utils/SessionManager.cpp
@@ -1,5 +1,10 @@
 #include "SessionManager.hpp"
 SessionManager::SessionManager() {}
-SessionManager::SessionManager(const SessionManager& other) {}
-SessionManager& SessionManager::operator=(const SessionManager& other) {}
+SessionManager::SessionManager(const SessionManager& other) {
+    (void)other;
+}
+SessionManager& SessionManager::operator=(const SessionManager& other) {
+    (void)other;
+    return *this;
+}
 SessionManager::~SessionManager() {}
