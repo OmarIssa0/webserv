22752482da0bee37ef1b17b2e84961560497a937
root <loay1alhindi1@gmail.com>
2026-03-02 00:28:47 +0300

    clean: files and folder

--- .www/cgi-bin/env.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-
-my $body = do { local $/; <STDIN> };
-
-print "Content-Type: text/html\r\n\r\n";
-print "<h1>Perl CGI OK</h1>";
-print "<pre>$body</pre>";
